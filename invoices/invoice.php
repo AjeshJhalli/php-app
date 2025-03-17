56e239fb00fe796f94db565b5bb8c6f828beb952
Ajesh Jhalli <ajeshjhalli@mail.com>
2025-03-17 21:22:25 +0000

    Removr postgres

--- invoices/invoice.php
+++ invoices/invoice.php
@@ -1,39 +1,27 @@
 <?php
 
 include "../functions/format_currency.php";
+include "../core-signed-in.php";
 
-$url_parts = explode('?', $_SERVER['REQUEST_URI']);
-$url_path = $url_parts[0];
-
-session_start();
-
-if (!isset($_SESSION['logged_in']) && $url_path != "/auth/signin.php") {
-  header('Location: /auth/signin.php');
-  die();
-}
-
-$dbconn = pg_connect("user=postgres.wjucgknzgympnnywamjy password=" . getenv("PGPASSWORD") . " host=aws-0-eu-west-2.pooler.supabase.com port=6543 dbname=postgres") or die('Could not connect: ' . pg_last_error());
-
-$invoice_id = $_GET["id"];
 $user_id = $_SESSION["id"];
+$invoice_id = $_GET["id"];
 
-$query = "
+$stmt = $db->prepare("
   SELECT status, sale.customer_id AS customer_id, sale.project_id AS project_id, sale.id AS sale_id, customer.name AS customer_name, project.name as project_name
   FROM sale
   LEFT JOIN customer
   ON customer.id = sale.customer_id
   LEFT JOIN project
   ON project.id = sale.project_id
-  WHERE sale.id = $1 AND sale.user_id = $2";
-$params = [$invoice_id, $user_id];
-$result = pg_query_params($dbconn, $query, $params) or die('Query failed: ' . pg_last_error());
+  WHERE sale.id = ? AND sale.user_id = ?");
 
-if (!($line = pg_fetch_row($result, null, PGSQL_ASSOC))) {
+$stmt->execute([$invoice_id, $user_id]);
+$line = $stmt->fetch();
+
+if (!$line) {
   http_response_code(404);
   die();
 }
-
-pg_free_result($result);
 
 ?>
 
@@ -43,11 +31,7 @@ pg_free_result($result);
 <?php include "../head.html" ?>
 
 <body>
-  <?php
-  $uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-  $uri_segments = explode('/', $uri_path);
-  include "../nav.php";
-  ?>
+  <?php include "../nav.php"; ?>
   <main class="container my-5">
     <div class="d-flex justify-content-between align-items-center">
       <nav aria-label="breadcrumb">
@@ -56,9 +40,6 @@ pg_free_result($result);
           <li class="breadcrumb-item active" aria-current="page"><?php echo "#" . htmlspecialchars($invoice_id) ?></li>
         </ol>
       </nav>
-      <a class="btn btn-primary" href="/invoices/preview.php?id=<?php echo htmlspecialchars($invoice_id) ?>">
-        Preview
-      </a>
     </div>
     <h2 class="py-4">Invoice #<?php echo htmlspecialchars($line['sale_id']) ?></h2>
     <table class="table mb-5">
@@ -96,31 +77,29 @@ pg_free_result($result);
             <select name="address_id" class="form-select form-select-sm" hx-post="/invoices/customer-address.php" hx-swap="none" hx-include="next input">
               <?php
 
-              $query = "
-          SELECT
-              address.id as address_id,
-              line1, 
-              line2, 
-              city, 
-              county, 
-              country, 
-              postcode,
-              CASE 
-                  WHEN sale_with_address.id IS NOT NULL THEN true
-                  ELSE false
-              END AS has_address_set
-          FROM address
-          INNER JOIN sale 
-              ON sale.customer_id = address.customer_id
-          LEFT JOIN sale AS sale_with_address
-              ON sale_with_address.customer_address_id = address.id
-          WHERE sale.user_id = $1 AND sale.id = $2;
-        ";
+              $stmt = $db->prepare("
+                SELECT
+                    address.id as address_id,
+                    line1,
+                    line2, 
+                    city, 
+                    county, 
+                    country, 
+                    postcode,
+                    CASE 
+                        WHEN sale_with_address.id IS NOT NULL THEN true
+                        ELSE false
+                    END AS has_address_set
+                FROM address
+                INNER JOIN sale 
+                    ON sale.customer_id = address.customer_id
+                LEFT JOIN sale AS sale_with_address
+                    ON sale_with_address.customer_address_id = address.id
+                WHERE sale.user_id = $1 AND sale.id = $2;
+              ");
+              $stmt->execute([$user_id, $invoice_id]);
 
-              $params = [$user_id, $invoice_id];
-              $result = pg_query_params($dbconn, $query, $params) or die('Query failed: ' . pg_last_error());
-
-              while ($row = pg_fetch_assoc($result)) { ?>
+              foreach ($stmt as $row) { ?>
                 <option value="<?php echo $row["address_id"] ?>" <?php if ($row["has_address_set"] === "t") echo "selected"; ?>>
                   <?php echo $row["line1"]; ?>,
                   <?php echo $row["city"]; ?>,
@@ -152,9 +131,10 @@ pg_free_result($result);
       <tbody>
         <?php
 
-        $result = pg_query_params($dbconn, "SELECT name, quantity, unit_amount FROM sale_line_item WHERE sale_id = $1 AND user_id = $2", [$invoice_id, $user_id]);
+        $stmt = $db->prepare("SELECT name, quantity, unit_amount FROM sale_line_item WHERE sale_id = ? AND user_id = ?");
+        $stmt->execute([$invoice_id, $user_id]);
 
-        while ($line_item = pg_fetch_assoc($result)) { ?>
+        foreach ($stmt as $line_item) { ?>
           <tr>
             <td><?php echo htmlspecialchars($line_item["name"]); ?></td>
             <td style="text-align: right;"><?php echo htmlspecialchars($line_item["quantity"]); ?></td>
@@ -170,14 +150,18 @@ pg_free_result($result);
           <th style="text-align: right;">Grand Total:</th>
           <th style="text-align: right;">
             <?php
-            $result = pg_query_params($dbconn, "SELECT SUM(sale_line_item.unit_amount * sale_line_item.quantity) AS amount
+
+            $stmt = $db->prepare("SELECT SUM(sale_line_item.unit_amount * sale_line_item.quantity) AS amount
             FROM sale
             LEFT JOIN sale_line_item
             ON sale_line_item.sale_id = sale.id
             WHERE sale.user_id = $2 AND sale.id = $1
-            GROUP BY sale.id", [$invoice_id, $user_id]);
-            $row = pg_fetch_assoc($result);
+            GROUP BY sale.id");
+            $stmt->execute([$invoice_id, $user_id]);
+            $row = $stmt->fetch();
+
             echo format_currency($row["amount"]);
+
             ?>
           </th>
         </tr>
@@ -187,5 +171,3 @@ pg_free_result($result);
 </body>
 
 </html>
-
-<?php pg_close($dbconn);
