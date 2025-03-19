90f3791b9b1bb4e90821b0a89d7ef707f760f5c3
Ajesh Jhalli <ajeshjhalli@mail.com>
2025-03-19 20:43:52 +0000

    Use DataTables for invoices

--- invoices.php
+++ invoices.php
@@ -1,8 +1,45 @@
 <?php
 
 include "./core-signed-in.php";
+include "./functions/format_currency.php";
 
-$new = isset($_GET['mode']) && $_GET['mode'] === 'new';
+$user_id = $_SESSION["id"];
+
+if (isset($_GET["search"])) {
+  $search = $_GET["search"];
+  $search = "%$search%";
+  $query = "
+    SELECT sale.id AS id, sale.status AS status, customer.name AS customer_name, project.name AS project_name, SUM(sale_line_item.unit_amount * sale_line_item.quantity) AS amount
+    FROM sale
+    LEFT JOIN project
+    ON project.id = sale.project_id
+    LEFT JOIN customer
+    ON customer.id = sale.customer_id
+    LEFT JOIN sale_line_item
+    ON sale_line_item.sale_id = sale.id
+    WHERE sale.user_id = :user_id AND
+    (project.name LIKE :search OR customer.name LIKE :search)
+    GROUP BY sale.id, customer_name, project_name
+  ";
+  $params = [":user_id" => $user_id, ":search" => $search];
+} else {
+  $query = "
+    SELECT sale.id AS id, sale.status AS status, customer.name AS customer_name, project.name AS project_name, SUM(sale_line_item.unit_amount * sale_line_item.quantity) AS amount
+    FROM sale
+    LEFT JOIN project
+    ON project.id = sale.project_id
+    LEFT JOIN customer
+    ON customer.id = sale.customer_id
+    LEFT JOIN sale_line_item
+    ON sale_line_item.sale_id = sale.id
+    WHERE sale.user_id = ?
+    GROUP BY sale.id, customer_name, project_name
+  ";
+  $params = [$user_id];
+}
+
+$stmt = $db->prepare($query);
+$stmt->execute($params);
 
 ?>
 
@@ -14,25 +51,49 @@ $new = isset($_GET['mode']) && $_GET['mode'] === 'new';
   <?php include "./nav.php"; ?>
   <main class="container my-5">
     <h1 class="mb-4">Invoices</h1>
-    <div class="d-flex justify-content-between align-items-center mb-3">
-        <form class="form-inline my-2 my-lg-0 d-flex" hx-get="/components/invoice-list.php" hx-target="#project-tbody" hx-trigger="load, input changed delay:500ms, search">
-          <input class="form-control" name="search" type="search" placeholder="Search" aria-label="Search">
-        </form>
-      </div>
-      <table class="table border mt-2 table-hover">
-        <thead>
-          <tr>
-            <th scope="col" style="text-align: right; padding-right: 40px; width: 200px;">Invoice Number</th>
-            <th scope="col">Customer</th>
-            <th scope="col">Project</th>
-            <th scope="col">Status</th>
-            <th scope="col" style="text-align: right; padding-right: 40px; width: 200px;">Amount</th>
+    <table id="table-invoices" class="display">
+      <thead>
+        <tr>
+          <th scope="col" style="text-align: right; padding-right: 40px; width: 200px;">Invoice Number</th>
+          <th scope="col">Customer</th>
+          <th scope="col">Project</th>
+          <th scope="col">Status</th>
+          <th scope="col" style="text-align: right; padding-right: 40px; width: 200px;">Amount</th>
+        </tr>
+      </thead>
+      <tbody id="project-tbody" hx-get="/components/invoice-list.php" hx-trigger="load">
+        <?php foreach ($stmt as $line) { ?>
+          <tr onclick="window.location.href = '/invoices/invoice.php?id=<?php echo htmlspecialchars($line["id"]); ?>'">
+            <td style="text-align: right; padding-right: 40px;">
+              <?php echo htmlspecialchars($line['id']); ?>
+            </td>
+            <td>
+              <?php echo htmlspecialchars($line['customer_name']); ?>
+            </td>
+            <td>
+              <?php echo htmlspecialchars($line['project_name']); ?>
+            </td>
+            <td>
+              <?php echo htmlspecialchars($line['status']); ?>
+            </td>
+            <td style="text-align: right; padding-right: 40px;">
+              <?php
+              if ($line["amount"]) {
+                $amount = $line["amount"];
+              } else {
+                $amount = 0;
+              }
+              echo format_currency($amount);
+              ?>
+            </td>
           </tr>
-        </thead>
-        <tbody id="project-tbody">
-        </tbody>
-      </table>
+        <?php } ?>
+      </tbody>
+    </table>
   </main>
+  <script defer>
+    new DataTable('#table-invoices');
+  </script>
 </body>
 
 </html>
